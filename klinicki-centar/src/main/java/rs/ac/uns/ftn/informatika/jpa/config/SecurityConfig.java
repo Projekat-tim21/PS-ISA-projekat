1922ff1e781db1a847633e4b609940acd3365126
jelenajelena11 <stojanovicj97@gmail.com>
2019-11-24 17:14:26 +0100

    Dodata autentifikacija

---
+++ klinicki-centar/src/main/java/rs/ac/uns/ftn/informatika/jpa/config/SecurityConfig.java
@@ -0,0 +1,37 @@
+package rs.ac.uns.ftn.informatika.jpa.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+//@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+	
+	@Override
+	public void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication().withUser("user")
+				.password("password").roles("USER");
+	}
+	
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		http
+			.authorizeRequests().antMatchers("/").permitAll()
+			.antMatchers("/login").permitAll()
+			.antMatchers("/registracija").permitAll()
+			.anyRequest()
+			.authenticated();
+		http.csrf().disable();
+			//.and()
+			/*.formLogin().loginPage("/login.jsp")
+			.failureUrl("/login.jsp?error=1").loginProcessingUrl("/login")
+			.permitAll().and().logout()
+			.logoutSuccessUrl("/listEmployees.html");*/
+
+	}
+
+}
