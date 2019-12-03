28b4296ff040091a1303c6e701fc1083970e8ec6
jelenajelena11 <stojanovicj97@gmail.com>
2019-12-03 23:06:48 +0100

    Dodat pregled

---
+++ klinicki-centar/src/main/java/rs/ac/uns/ftn/informatika/jpa/service/PregledService.java
@@ -0,0 +1,43 @@
+package rs.ac.uns.ftn.informatika.jpa.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import rs.ac.uns.ftn.informatika.jpa.model.Pregled;
+import rs.ac.uns.ftn.informatika.jpa.repository.PregledRepository;
+
+@Service
+@Transactional
+public class PregledService {
+	
+	@Autowired
+	private PregledRepository pregledRepo;
+	
+	public PregledService(PregledRepository pregledRepo) {
+		this.pregledRepo = pregledRepo;
+	}
+	
+	public List<Pregled> findAll(){
+		List<Pregled> allP = new ArrayList<Pregled>();
+		for(Pregled pregled : pregledRepo.findAll()) {
+			allP.add(pregled);
+		}
+		return allP;
+	}
+	
+	public Pregled findById(Long id) {
+		
+		return pregledRepo.findOneById(id);
+	}
+	
+	/*public Pregled findByDate(String date) {
+		
+		return pregledRepo.findByDate(date);
+	}
+*/
+}
