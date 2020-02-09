bb57d3f16fb86da7ebd6f1f01f7ef3d92761a008
Projekat-tim21 <vanjajeftic97@gmail.com>
2020-02-09 11:56:45 +0100

    ispravka greske dupliranja od strane master grane

--- klinicki-centar/src/main/java/rs/ac/uns/ftn/informatika/jpa/dto/PregledDTO.java
+++ klinicki-centar/src/main/java/rs/ac/uns/ftn/informatika/jpa/dto/PregledDTO.java
@@ -1,13 +1,7 @@
 package rs.ac.uns.ftn.informatika.jpa.dto;
 
 
-import rs.ac.uns.ftn.informatika.jpa.model.Operacija;
 import rs.ac.uns.ftn.informatika.jpa.model.Pregled;
-
-import javax.persistence.Column;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 
 public class PregledDTO {
@@ -157,34 +151,6 @@ public class PregledDTO {
 		this.korisnikId = korisnikId;
 	}
 
-	private Boolean obradjen;	
-
-	private String datum;   //ovo se ne koristi vise
-
-	private String vreme;   //ovo se ne koristi vise
-	
-	private String tip;
-	
-	private String lekarimepregled;
-
-	private String lekarprezimepregled;
-	
-	private String lekar;  //ovo se ne koristi vise
-
-	private int cena;
-
-	private double ocenapregleda;
-
-
-	private String trajanje;
-	
-	private String sala;
-	
-	private Boolean zakazan;
-	
-	private boolean obavljenpregled;
-
-	private Long korisnikId;
 	
 	
 	public Long getId() {
